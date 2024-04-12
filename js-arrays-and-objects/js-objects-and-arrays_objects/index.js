0bb928b98751215b66e0896a70cf072973780b76
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-12 12:19:30 +0200

    update objects

--- js-arrays-and-objects/js-objects-and-arrays_objects/index.js
+++ js-arrays-and-objects/js-objects-and-arrays_objects/index.js
@@ -4,8 +4,11 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "Conall",
+  age: 28,
+  email: "myEmail@email.com",
 };
+console.log("Excercise 1: ", personWithNameAgeEmail);
 
 // EXERCISE 2
 // Set the values of name and age equal to the corresponding values of the personAlex object.
@@ -15,8 +18,10 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
+
+console.log("Name: ", nameOfAlex, "\n", "Age: ", ageOfAlex);
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -26,6 +31,11 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.name = "Alex";
+personToChange.age = 35;
+
+console.log("Excercise 3: ", personToChange);
+
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 
@@ -33,6 +43,11 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+petPluto.age = 5;
+petPluto.breed = "Husky";
+
+console.log("Excercise 4: ", petPluto);
 
 export {
   personWithNameAgeEmail,
