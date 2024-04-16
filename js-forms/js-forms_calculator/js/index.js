0e6c98a72cf0503a40b6ab7b0b360e01655e515f
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-16 21:23:57 +0200

    add math operators to event listener

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -2,6 +2,9 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
+const numberA = document.getElementById("number-a");
+const numberB = document.getElementById("number-b");
+const operator = document.getElementsByName("operator");
 
 function add(a, b) {
   return a + b;
@@ -25,6 +28,21 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  const numA = Number(numberA.value);
+  const numB = Number(numberB.value);
+  if (operator[0].checked) {
+    console.log("Add");
+    result = add(numA, numB);
+  } else if (operator[1].checked) {
+    console.log("Subtract");
+    result = subtract(numA, numB);
+  } else if (operator[2].checked) {
+    console.log("Multiply");
+    result = multiply(numA, numB);
+  } else if (operator[3].checked) {
+    console.log("Divide");
+    result = divide(numA, numB);
+  }
 
   // --^-- write your code here --^--
 
