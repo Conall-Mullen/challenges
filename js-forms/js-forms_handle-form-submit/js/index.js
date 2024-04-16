9a0c89699442b0c7a79c3c292e994fc485818568
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-16 11:26:54 +0200

    add complaint object

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,27 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const ageInput = document.getElementById("age");
+const emailInput = document.getElementById("email");
+const complaintInput = document.getElementById("complaint");
+const detailsInput = document.getElementById("details");
+const badnessInput = document.getElementById("badness");
+const dateInput = document.getElementById("order-date");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let complaintFile = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    age: ageInput.value,
+    email: emailInput.value,
+    complaint: complaintInput.value,
+    details: detailsInput.value,
+    rating: badnessInput.value,
+    date: dateInput.value,
+  };
+
+  console.log(complaintFile);
+});
