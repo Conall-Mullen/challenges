13f8db1981e2b63db494e035ad114b0a7573cde2
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-17 16:35:08 +0200

    add event listener to call renderStars with correct number

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -8,16 +8,20 @@ function renderStars(filledStars) {
 
   //--v-- your code here --v--
   for (let i = 1; i <= 5; i++) {
-    const image = document.createElement("img");
+    //loop 5 times
+    const image = document.createElement("img"); //create image element each iteration
+    image.setAttribute("alt", i);
     if (i <= filledStars) {
       image.setAttribute("src", "assets/star-filled.svg");
     } else {
       image.setAttribute("src", "assets/star-empty.svg");
     }
-
+    image.addEventListener("click", () => {
+      renderStars(image.alt);
+    });
     starContainer.append(image);
   }
+
   //--^-- your code here --^--
 }
-
-renderStars(1);
+renderStars();
