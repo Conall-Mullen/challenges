816dd80bf6da5bc49973932bb74cc741a1100d37
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-17 15:36:28 +0200

    add for in loop with language options

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -21,7 +21,12 @@ select.name = "languages";
 main.append(select);
 
 // --v-- write/change code here --v--
-
+for (const key in languages) {
+  const option = document.createElement("option");
+  option.textContent = languages[key];
+  select.append(option);
+  console.log(languages[key]);
+}
 // --^-- write/change code here --^--
 
 // Part 2: Creating a Navigation Bar
