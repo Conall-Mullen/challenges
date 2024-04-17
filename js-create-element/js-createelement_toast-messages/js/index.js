dcba48b5fa87ce133c455be5d5f8691af3608c4b
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-17 12:19:21 +0200

    add and remove toast messages via event listeners

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,14 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+
+  const toastMessage = document.createElement("li");
+  toastMessage.textContent = `I am a new toast message`;
+  toastMessage.classList.add("toast-container__message");
+  toastContainer.append(toastMessage);
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", (e) => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
