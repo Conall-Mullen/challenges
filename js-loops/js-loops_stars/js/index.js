2ce6a46e523998ec145cdbd85801bd89bfdd5605
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-17 16:08:27 +0200

    add empty stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -7,7 +7,11 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const image = document.createElement("img");
+    image.setAttribute("src", "assets/star-empty.svg");
+    starContainer.append(image);
+  }
   //--^-- your code here --^--
 }
 
