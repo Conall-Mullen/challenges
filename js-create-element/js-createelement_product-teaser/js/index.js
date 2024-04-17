1e6470c2cd6705a6ab3e4a1121dab680da8fb761
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-17 13:57:22 +0200

    add second article and event listener

--- js-create-element/js-createelement_product-teaser/js/index.js
+++ js-create-element/js-createelement_product-teaser/js/index.js
@@ -7,5 +7,49 @@ const category1 = "Freshwater";
 const category2 = "Large aquarium";
 const category3 = "Plankton Diet";
 const price = "149,99 €";
+
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const body = document.getElementsByTagName("body");
+const buyButton = document.getElementsByClassName("product__buy-button");
+console.log(buyButton);
+
+document.body.innerHTML += `<article class="product">
+<div class="product__body">
+  <div class="product__text-container">
+    <h2 class="product__name">${name}</h2>
+    <ul class="product__categories">
+      <li class="product__category">${category1}</li>
+      <li class="product__category">${category2}</li>
+      <li class="product__category">${category3}</li>
+    </ul>
+    <p class="product__description">
+    ${description}
+    </p>
+  </div>
+  <div class="product__image-container">
+    <img
+      class="product__image"
+      src=${imageSrc}
+      alt=""
+    />
+  </div>
+</div>
+<footer class="product__footer">
+  <span class="product__price">${price}</span>
+  <button type="button" class="product__buy-button">Buy</button>
+</footer>
+</article>`;
+
+buyButton[0].addEventListener("click", () => {
+  const price = document.getElementsByClassName("product__price");
+
+  console.log("Price 1: ", price[0].textContent);
+});
+
+buyButton[1].addEventListener("click", () => {
+  const price = document.getElementsByClassName("product__price");
+
+  console.log("Price 2: ", price[1].textContent);
+});
