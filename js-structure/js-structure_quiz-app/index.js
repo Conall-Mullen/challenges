5108ffa1e5baab6a9900c3e6ef8ed5f00feca5d9
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-25 16:37:20 +0200

    remove unnecessary imports from  index.js

--- js-structure/js-structure_quiz-app/index.js
+++ js-structure/js-structure_quiz-app/index.js
@@ -1,11 +1,5 @@
 // Store the cards in a global state
-import { cards } from "./components/CardList/CardList.js";
-import { CardList } from "./components/CardList/CardList.js";
-import { Header } from "./components/Header/Header.js";
-import { handleFormSubmit } from "./components/Form/Form.js";
-import { Form } from "./components/Form/Form.js";
-import { Bookmark } from "./components/Bookmark/Bookmark.js";
-import { Card } from "./components/Card/Card.js";
+
 import { App } from "./components/App/App.js";
 
 // The render function renders the app to the DOM
