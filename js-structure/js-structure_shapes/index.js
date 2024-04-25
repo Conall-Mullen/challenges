98643ef62e427c7c52e90923aabe066a4d0420cf
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-25 16:41:08 +0200

    remove unnecessary import from index.js

--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,5 +1,5 @@
 console.clear();
-import { getRandomColor as randomColor } from "./utils/randomColor.js";
+
 import { circleElement } from "./components/Circle/Circle.js";
 import { squareElement } from "./components/Square/Square.js";
 import { pentagonElement } from "./components/Pentagon/Pentagon.js";
