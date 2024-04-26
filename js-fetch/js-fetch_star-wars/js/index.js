bb83e36d9f5e0f72d46cc59a0c93661474c0015e
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-26 13:51:44 +0200

    aadd
    add fetch

--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -37,10 +37,15 @@ const EXAMPLE_DATA = {
 const firstCard = Card(EXAMPLE_DATA);
 renderElement(firstCard);
 
-fetchDataAndRender();
-
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+export async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people/");
+  const data = await response.json();
+  return data.results;
 }
+const fetchedData = await fetchDataAndRender();
+
+fetchedData.forEach((datum) => {
+  renderElement(Card(datum));
+});
