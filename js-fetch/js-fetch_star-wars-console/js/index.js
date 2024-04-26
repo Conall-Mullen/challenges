f39f0c957f0b29486d7d87de455277ae762b6b34
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-26 11:46:29 +0200

    fetch api data and filter results

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,28 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.py4e.com/api/people/";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const characters = data.results;
+      const result = characters.filter(
+        (character) => character.name === "R2-D2"
+      );
+      console.log(result[0].eye_color);
+
+      //   characters.forEach((character) => {
+      //     if (character.name === "R2-D2") {
+      //       console.log(character.eye_color);
+      //     }
+      //   });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 fetchData();
