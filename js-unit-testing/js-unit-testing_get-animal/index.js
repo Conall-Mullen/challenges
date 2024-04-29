2da6a295f415755552f1e9856b2f409209a95d3c
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-29 10:50:23 +0200

    add getAnimal function

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(name) {
+  if (name === "cats") {
+    return `I totally love ${name}!`;
+  } else if (name === undefined) {
+    return `I do not like animals at all!`;
+  } else {
+    return `I like ${name}!`;
+  }
+}
