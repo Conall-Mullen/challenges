c2cc9f99ff727374403ae7f40f384551c1cedc3b
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-29 14:22:03 +0200

    add more functions

--- js-unit-testing/js-unit-testing_people/index.js
+++ js-unit-testing/js-unit-testing_people/index.js
@@ -1,19 +1,79 @@
+const people = [
+  {
+    id: 1,
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 21,
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+  },
+  {
+    id: 3,
+    firstName: "Max",
+    lastName: "Mustermann",
+    age: 32,
+  },
+  {
+    id: 4,
+    firstName: "Erika",
+    lastName: "Musterfrau",
+    age: 38,
+  },
+  {
+    id: 5,
+    firstName: "Luke",
+    lastName: "Skywalker",
+    age: 23,
+  },
+];
+
 export function getFirstNames(people) {
+  console.log(people.map((person) => person.firstName));
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  console.log(people.map((person) => `${person.firstName} ${person.lastName}`));
+  return people.map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  console.log(
+    people.map((person) => `Name: ${person.firstName} Age:  ${person.age}`)
+  );
 
-export function getPeopleByAge(people, age) {}
+  return people.map(
+    (person) => `Name: ${person.firstName} Age:  ${person.age}`
+  );
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleByAge(people, age) {
+  console.log(people.filter((person) => person.age === age));
+  return people.filter((person) => person.age === age);
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleNamesOlderThan(people, age) {
+  console.log(people.filter((person) => person.age > age));
+  return people.filter((person) => person.age > age);
+}
+
+export function getPeopleByLastName(people, lastName) {
+  console.log(people.filter((person) => person.lastName === lastName));
+  return people.filter((person) => person.lastName === lastName);
+}
 
 export function findPersonById(people, id) {}
 
 export function isAnyoneOlderThan(people, age) {}
 
 export function getFullNamesSortedByAge(people) {}
+
+getFullNames(people);
+getNameAndAge(people);
+getPeopleByAge(people, 21);
+getPeopleNamesOlderThan(people, 21);
+getPeopleByLastName(people, "Skywalker");
