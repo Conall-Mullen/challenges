5f74cbca3ef1ee594713b552249c584e139dade8
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-06 17:16:15 +0200

    add fragment

--- react-nesting/react-nesting_fragments/src/App.jsx
+++ react-nesting/react-nesting_fragments/src/App.jsx
@@ -4,9 +4,17 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <Boxes />
     </main>
+  );
+}
+
+function Boxes({ color }) {
+  return (
+    <>
+      <Box color="#007bff"></Box>
+      <Box color="#fc3"></Box>
+      <Box color="#ff3333"></Box>
+    </>
   );
 }
