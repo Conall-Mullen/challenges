916f10f462e2fa873a1d8173ab50a49b63fbd9b0
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-06 16:26:24 +0200

    add props

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Like</Button>
+      <Button>Comment</Button>
+      <Button>Share</Button>
+      <Button>Report</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
