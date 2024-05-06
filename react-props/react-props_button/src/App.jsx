6eef8d3b6b94dcc5fc636da69285755ca1a88bc7
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-06 12:27:46 +0200

    add click event listener

--- react-props/react-props_button/src/App.jsx
+++ react-props/react-props_button/src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button color={"#f31601"} disabled={false} text={"hello"} />;
+}
+
+function Button({ color, disabled, text, onClick }) {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+  return (
+    <button disabled={disabled} style={{ color: color }} onClick={handleClick}>
+      {text}
+    </button>
+  );
 }
