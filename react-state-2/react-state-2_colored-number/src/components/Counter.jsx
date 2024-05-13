a7545f498ea317212dbd0478f7a4f6186719ab68
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-13 11:57:08 +0200

    add state, increment and decrement functions

--- react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 import "./Counter.css";
 
-
 export default function Counter() {
+  let [value, setValue] = useState(0);
+
+  function increment() {
+    setValue(++value);
+  }
+  function decrement() {
+    setValue(--value);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={value} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={increment}
         >
           +
         </button>
@@ -19,6 +28,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={decrement}
         >
           -
         </button>
